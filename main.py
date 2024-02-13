3906182a50cdf471cb856b06221f968ad43e1161
Chris <cgbollands@gmail.com>
2024-02-13 14:07:00 +1100

    added files

---
+++ main.py
@@ -0,0 +1,21 @@
+import configparser
+import os
+
+config = configparser.ConfigParser()
+
+def move_raw():
+    return
+
+def move_jpg():
+    return
+
+def move_vids():
+    return
+
+def main():
+    move_raw()
+    move_jpg()
+    move_vids()
+
+if __name__ == "__main__":
+    main()
